161e0d1b24bf160b22cc0809dec967ed04787b37
OpRG2 <105670901+OpRG2@users.noreply.github.com>
2022-05-17 12:00:15 +0530

    config.php

--- web/config.php
+++ web/config.php
@@ -22,11 +22,16 @@ else {
 }
 
 $GLOBALS["START_MESSAGE"] = <<<EOM
-Thank you for using me <a href="https://github.com/skrtdev/FilesBot">😬</a>
+Thank you for using me <a href="https://t.me/RG_Anime_Group">😬</a>
 
 <u><b>you can forward me any media message</b></u>, and <b><i>I might help you to create a PUBlic link</i></b>.
 
-Subscribe ℹ️ @skrtdev if you ❤️ using this bot!
+Subscribe ℹ️ @RG_Anime_Group if you ❤️ using this bot!
 EOM;
+reply_markup=InlineKeyboardMarkup(
+          [
+                    InlineKeyboardButton(
+                        "ʜᴇʟᴘ & ᴄᴏᴍᴍᴀɴᴅꜱ", url=f"https://t.me/RG_Anime_Group"
+                    ),
 $GLOBALS["CHECKING_MESSAGE"] = "🤔";
 require_once __DIR__ . "/../vendor/autoload.php";
